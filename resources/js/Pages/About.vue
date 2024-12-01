a43061d9810e3adca2924453f4d74f07a5e1c4da
Clepher Jaudian <clepher.jaudian@msunaawan.edu.ph>
2024-12-01 15:52:21 +0800

    different type of Routes setup

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,13 @@
+<script setup>
+    defineProps({
+        user: String
+    })
+</script>
+
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            About Page {{ user }}
+        </h1>
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home');
 });
+
+// Route::get('/about', function(){
+//     return inertia('About', ['user' => 'Clepher']);
+// });
+
+// or
+Route::inertia('/about', 'About', ['user' => 'Clepher']);
